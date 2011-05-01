e9bf1c46beb3bd5cb442abcfa961afffd45bbdd0
Ken Burkhardt <ken.burkhardt@gmail.com>
2011-05-01 15:48:03 -0400

    initial dot net project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DesignPatternsShared.ChapterOne;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallard = new MallardDuck();
+            mallard.performQuack();
+            mallard.performFly();            
+
+            Console.Read();
+        }
+    }
+}
